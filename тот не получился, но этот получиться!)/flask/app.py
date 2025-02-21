b1abddde0775bbd8fff15ac72c10e9520deb713d
genius of the whole world <144894645+geniusss96@users.noreply.github.com>
2025-02-21 18:44:35 +0500

    www

--- тот не получился, но этот получиться!)/flask/app.py
+++ тот не получился, но этот получиться!)/flask/app.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+data = ["Flask", "Python", "Web", "Backend", "API"]  # Данные для поиска
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")  # Загружаем HTML-шаблон
-    return render_template("style.css")  # Загружаем HTML-шаблон
+    query = request.form.get("search")  # Получаем текст из input
+    results = [item for item in data if query and query.lower() in item.lower()] if query else data
+    return render_template("index.html", results=results)
 
 if __name__ == "__main__":
     app.run(debug=True)
